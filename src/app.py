df8a7fd528a7c5264fc2d48c146408548f4e00f7
snigdhrf <snigdhsingh94@gmail.com>
2026-04-14 20:52:06 +0200

    Initial commit: basic

---
+++ src/app.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/hello-world")
+def hello_world():
+    return {"message": "Hello, World!"}
+
+@app.get("/print-this")
+def print_it():
+    return {"message": "nioagebgoiueabigtltiaegb"}
+
